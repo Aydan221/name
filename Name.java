dde3b439b3af14bf33d4b5c3c9dfe06abcf3dc8d
Aydan221 <129885253+Aydan221@users.noreply.github.com>
2023-04-13 09:14:36 -0700

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class main {
+  public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  System.out.println("Enter a name :");
+  String guess = input.next();
+  switch(guess){
+    case"aydan":
+    System.out.println(No way are you using hacks .");
+    break;
+  case "Ethan":
+    System.out.println("No way that not my not its like a loser name.");
+    break;
+default:
+    System.out.println("That is not a name i know.");
+}}
